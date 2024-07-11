f5c554a8bedfe884f41bf4fd60286cc0deab95d5
Satwik Bhasin <satwikbhasin@gmail.com>
2024-07-10 21:10:43 -0400

    spinner changed for upload

--- frontend/src/components/UploadFrom.tsx
+++ frontend/src/components/UploadFrom.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { nanoid } from 'nanoid';
-import { Clipboard, ClipboardCheck, CheckCircle, Upload } from 'lucide-react';
+import { Clipboard, ClipboardCheck, CheckCircle, Upload, CircleDashed } from 'lucide-react';
 import Cookies from 'js-cookie';
 
 import errors from '../assets/validationErrors';
@@ -140,10 +140,7 @@ const UploadForm: React.FC = () => {
                 <div className="grid gap-2 justify-center">
                     {isSubmitting ? (
                         <div className="flex justify-center items-center">
-                            <svg className="animate-spin h-7 w-7 text-emerald-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                                <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                            </svg>
+                            <CircleDashed className="animate-spin h-7 w-7 text-emerald-500" />
                         </div>
                     ) : (
                         <button
